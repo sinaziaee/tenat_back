6ed5041e0f68414705312ac0d9f3128f5a4e58de
haminob48 <haminob48@gmail.com>
2021-08-20 06:48:47 -0700

    Remove english stop words script in basic version

---
+++ scripts/stop_word_removal/english_stop_word_removal.py
@@ -0,0 +1,27 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+
+def run(from_path, to_path, name):
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    stop_words = set(stopwords.words('english'))
+    from_path = check_path.apply(from_path)
+    to_path = check_path.apply(to_path)
+    folder_path = f'media/result/{from_path}/{name}'
+    file_list = list_files.apply(folder_path)
+    folder_creator.apply(folder_path)
+    output_file_list = []
+    for file in file_list:
+        f = open(file, 'r', encoding='utf8')
+        file = str(file).replace(f'{from_path}', f'{to_path}')
+        f_output = open(file, 'w', encoding='utf8')
+        output_file_list.append(file)
+        text = f.read()
+        word_tokens = word_tokenize(text)
+        filtered_words = [w for w in word_tokens if not w.lower() in stop_words]
+        for r in filtered_words:
+            f_output.write(r + "\n")
+        f.flush()
+        f_output.flush()
+    return output_file_list
