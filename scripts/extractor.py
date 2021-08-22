91e52a7cdf933054bad177f72678adda8bf932ba
sinaziaee <sinaziaee99@gmail.com>
2021-08-22 12:33:40 +0430

    changed the extractor

--- scripts/extractor.py
+++ scripts/extractor.py
@@ -1,16 +1,8 @@
-import patoolib
 import os
 from pathlib import Path
 from scripts import list_files_and_sizes, folder_creator
 import docx2txt
-
-
-# from scripts import tfidf_per_doc, Ngram, wordCloud, LsiSimilarity, Definition, DocFeatures, wordCombGraph, \
-#     InvertedIndex, DocSimilarityByKeyword, word2gramGraph, Paragraphs
-# from scripts import DocumentList
-# from scripts import CreateFolder
-# from scripts import Graph
-# from scripts import StemDic
+import zip_unicode
 
 
 def apply(zip_file):
@@ -27,7 +19,8 @@ def apply(zip_file):
             path = Path(dirname).parent
             path = os.path.join(path, f'media/data/{folder_name}')
             os.mkdir(path)
-        patoolib.extract_archive(zip_file, outdir=folder_path, interactive=False)
+        zip_ref = zip_unicode.ZipHandler(path=zip_file, extract_path=folder_path)
+        zip_ref.extract_all()
     except OSError as error:
         print(error)
     files_list = []
@@ -62,36 +55,3 @@ def doc_to_txt(folder_path, file):
     f.write(text)
     f.flush()
 
-    # mapList = []
-
-    # for path, dirs, files in os.walk(folder_path):
-    #     for f in files:
-    #         fp = os.path.join(path, f)
-    #         # size += os.path.getsize(fp)
-    #         name = fp.split('/')[-1]
-    #         new_map = {name, os.path.getsize(fp)}
-    #         mapList.append(new_map)
-    #
-    # return mapList
-
-    # Manual set path in windows
-    # folder_name = "Fava_c"
-
-    # CreateFolder.apply(folder_name)
-    # tfidf_per_doc.start_tfidf(folder_name)
-    # DocumentList.apply(folder_name)
-    # Graph.apply(folder_name)
-    # StemDic.apply(folder_name)
-    # Ngram.apply(folder_name, 2)
-    # Ngram.apply(folder_name, 3)
-    # wordCloud.apply(folder_name)
-    # LsiSimilarity.apply(folder_name)
-    # Definition.apply(folder_name)
-    # DocFeatures.apply(folder_name)
-    # wordCombGraph.apply(folder_name)
-    # InvertedIndex.apply(folder_name)
-    # DocSimilarityByKeyword.apply(folder_name)
-    # word2gramGraph.apply(folder_name)
-    # Paragraphs.apply(folder_name)
-
-# extractor('media_cdn/docs/zips/fava.rar')
