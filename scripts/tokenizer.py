0cf7b854130ddcc4ae77e07c4752f56a7d7da58f
sinaziaee <sinaziaee99@gmail.com>
2021-08-23 17:11:47 +0430

    finished modifing tokenization

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -3,7 +3,7 @@ from scripts import list_files, folder_creator, check_path
 import hazm
 
 
-def apply(name, splitter, from_path, to_path):
+def apply(name, splitter, from_path, to_path, tokens_count):
     from_path = check_path.apply(from_path)
     to_path = check_path.apply(to_path)
     folder_path = f'media/result/{from_path}/{name}/'
@@ -15,7 +15,8 @@ def apply(name, splitter, from_path, to_path):
         f.flush()
         writer_hazm(folder_path, str(file), text, splitter)
     files_list = list_files.apply(folder_path)
-    return files_list
+    # return files_list
+    return show_files(files=files_list, tokens_count=tokens_count)
 
 
 def writer_hazm(folder_path, file, text, splitter):
@@ -24,7 +25,30 @@ def writer_hazm(folder_path, file, text, splitter):
     folder_creator.apply(folder_path)
     filename = str(file).replace('.docx', '.txt')
     f = open(os.path.join(folder_path, filename), 'w')
-    temp = hazm.word_tokenize(text)
+    # temp = hazm.word_tokenize(text)
+    temp = text.split(splitter)
     for each in temp:
-        f.write(f'{each}{splitter}')
+        f.write(f'{each}\n')
     f.flush()
+
+
+def show_files(files, tokens_count):
+    result_list = []
+    for each in files:
+        token_counter = 0
+        file_name = os.path.basename(str(each))
+        first_top_tokens = ''
+        f = open(str(each), 'r', encoding='utf-8')
+        for line in f.readlines():
+            line = line.replace('\n', '')
+            token_counter += 1
+            if token_counter < tokens_count:
+                first_top_tokens += line + ', '
+            elif token_counter == tokens_count:
+                first_top_tokens += line
+            else:
+                pass
+        new_map = {'doc_name': file_name, 'tokens_count': token_counter, 'tokens': first_top_tokens, 'link': each}
+        result_list.append(new_map)
+    return result_list
+
