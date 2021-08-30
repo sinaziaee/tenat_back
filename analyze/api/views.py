0e586501b61bc2dee06f41cecc6f7aaa746c31e1
haminob48 <haminob48@gmail.com>
2021-08-30 13:24:10 -0700

    Implement lemmatizer

--- analyze/api/views.py
+++ analyze/api/views.py
@@ -9,6 +9,7 @@ from scripts.normalize import english_normalizer, persian_normalizer
 from scripts.stem import english_stemmer, persian_stemmer
 from scripts.stop_word_removal import english_stop_word_removal, persian_stop_word_removal
 from scripts.doc_statistics import english_statistics, persian_statistics
+from scripts.lemmatize import english_lemmatizer, persian_lemmatizer
 from analyze.api.serializer import *
 import time
 
@@ -131,6 +132,24 @@ def stem(request):
         result = english_stemmer.apply(from_path=from_path, algorithm=algorithm, to_path='stemmed', name=name, token_count=token_count)
     else:
         result = english_stemmer.apply(from_path=from_path, algorithm=algorithm, to_path='stemmed', name=name, token_count=token_count)
+    if result is not None and len(result) != 0:
+        return Response(result, status=status.HTTP_200_OK)
+    return Response('failed', status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def lemmatize(request):
+    new_map = request.POST
+    name = new_map.get('name')
+    from_path = new_map.get('from')
+    language = new_map.get('language')
+    token_count = 10
+
+    if language == 'Persian':
+        result = english_lemmatizer.apply(from_path=from_path, to_path='lemmat', name=name, token_count=token_count)
+    elif language == 'English':
+        result = english_lemmatizer.apply(from_path=from_path, to_path='lemmat', name=name, token_count=token_count)
+    else:
+        result = english_lemmatizer.apply(from_path=from_path, to_path='lemmat', name=name, token_count=token_count)
     if result is not None and len(result) != 0:
         return Response(result, status=status.HTTP_200_OK)
     return Response('failed', status=status.HTTP_400_BAD_REQUEST)
--- scripts/lemmatize/persian_lemmatizer.py
+++ scripts/lemmatize/persian_lemmatizer.py
@@ -1,8 +1,47 @@
 import hazm
 from scripts import check_path, list_files, folder_creator, slicer
 from scripts import base_script
+from hazm import word_tokenize
 
-
-def apply(from_path, to_path, name):
+def lemmatize(text):
     lemmatizer = hazm.Lemmatizer()
-    return base_script.apply(from_path, to_path, name, lemmatizer.lemmatize)
+    words = word_tokenize(text)
+    lemm_text = lemmatizer.lemmatize(text)
+    all_lemm = []
+    for word in words:
+        lem = lemmatizer.lemmatize(word)
+        all_lemm.append(f'{word} : {lem}')
+    return {'text':lemm_text, 'lemmatized_words':all_lemm}
+
+def apply(from_path, to_path, name, token_count):
+    from_path = check_path.apply(from_path)
+    to_path = check_path.apply(to_path)
+    folder_path = f'media/result/{from_path}/{name}'
+    file_list = list_files.apply(folder_path)
+    folder_creator.apply(folder_path)
+    folder_path = f'media/result/{to_path}/{name}'
+    folder_creator.apply(folder_path)
+    folder_path = f'media/result/{to_path}/lemmatized_text_and_words/{name}'
+    folder_creator.apply(folder_path)
+    result_list = []
+    for file in file_list:
+        f = open(file, 'r', encoding='utf8')
+        doc_name = str(file).split('/')[-1].split('\\')[-1]
+        result_file = str(file).replace(f'{from_path}', f'{to_path}')
+        f_output = open(result_file, 'w', encoding='utf8')
+        text_result_file = str(file).replace(f'{from_path}', f'{to_path}/lemmatized_text_and_words')
+        f_text_output = open(text_result_file, 'w', encoding='utf8')
+        text = f.read()
+        result = lemmatize(text)
+        top_lemmed = " ,".join(result['lemmatized_words'][:token_count])
+        lemmed_text = result['text']
+        result_dict = {'top_tokens':top_lemmed, 'result_link':result_file, 'text_result_file':text_result_file, 'doc_name':doc_name}
+        f_output.write(f'{str(result_dict)}\n')
+        f_text_output.write(f'{lemmed_text}\n\n')
+        for lem in result['lemmatized_words']:
+            f_text_output.write(f'{lem}\n')
+        f.flush()
+        f_output.flush()
+        f_text_output.flush()
+        result_list.append(result_dict)
+    return result_list
