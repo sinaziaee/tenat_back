3dab14c934021e9dfdbdf3a9b07bf210b78e7975
haminob48 <haminob48@gmail.com>
2021-09-06 04:50:25 -0700

    Add complete path to export request

--- scripts/exporter.py
+++ scripts/exporter.py
@@ -3,6 +3,6 @@ from scripts import list_files, folder_creator, check_path
 
 
 def apply(from_path, format, name, to_path):
-    from_path = check_path.apply(from_path)
-    file_path = f'media/result/{from_path}/{name}/00_output_result.txt'
-    return file_path
+    #from_path = check_path.apply(from_path)
+    #file_path = f'media/result/{from_path}/{name}/00_output_result.txt'
+    return from_path + f'/00_output_result.txt'
