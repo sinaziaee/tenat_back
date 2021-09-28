35946699b8bfd80cc7e73f41ec6a066111a1a37a
armanheydari <RmanHeydari2000@gmail.co>
2021-09-28 19:44:57 +0330

    basic tf-idf output refactored

--- scripts/tf_idf/basic_tf_idf.py
+++ scripts/tf_idf/basic_tf_idf.py
@@ -6,15 +6,17 @@ import math
 
 nltk.download('punkt')
 
+
 def words_docs_frequency(document_word):
     words_in_docs = {}
-    for doc , words in document_word.items():
+    for doc, words in document_word.items():
         for word in words:
-            if not word in words_in_docs:
+            if word not in words_in_docs:
                 words_in_docs[word] = [doc]
-            elif not doc in words_in_docs[word]:
+            elif doc not in words_in_docs[word]:
                 words_in_docs[word].append(doc)
     return words_in_docs
+
 
 def words_per_document(docs):
     word_dict = {}
@@ -23,16 +25,18 @@ def words_per_document(docs):
         word_dict[doc] = dict(Counter(words))
     return word_dict
 
-def TF_IDF(word_dict,words_docs,doc_count,doc):
-    tf_idf_dict = {}
+
+def TF_IDF(word_dict, words_docs, doc_count, doc, file_name):
+    tf_idf_list = []
     all_words = len(word_tokenize(doc))
     for word, count in word_dict.items():
         frequency = count
         tf = frequency/all_words
         idf = math.log10(doc_count/len(words_docs[word]))
         tf_idf = tf * idf
-        tf_idf_dict[word] = {'frequency':frequency, 'tf':tf, 'idf':idf, 'tf_idf': tf_idf}
-    return tf_idf_dict
+        tf_idf_list.append({'term': word, 'doc': file_name, 'weight': tf_idf})
+    return tf_idf_list
+
 
 def apply(from_path, to_path, name):
     from_path = check_path.apply(from_path)
@@ -45,7 +49,6 @@ def apply(from_path, to_path, name):
     output_file_list = []
     doc_text_dict = {}
     doc_count = len(file_list)
-    print(file_list)
     for file in file_list:
         f = open(file, 'r', encoding='utf8')
         doc_text_dict[file] = f.read()
@@ -55,10 +58,8 @@ def apply(from_path, to_path, name):
         new_file = str(file).replace(f'{from_path}', f'{to_path}')
         output_file_list.append(new_file)
         f_output = open(new_file, 'w', encoding='utf8')
-        tf_idf = TF_IDF(word_dict[file], docs_per_word, doc_count, doc_text_dict[file])
+        file_name = str(file).split("\\")[-1]
+        tf_idf = TF_IDF(word_dict[file], docs_per_word, doc_count, doc_text_dict[file], file_name)
         f_output.write(str(tf_idf))
         f_output.flush()
-    return output_file_list
-
-
-        
+    return tf_idf
