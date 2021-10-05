e3a29cf6e3f9678d79f6fea5d89750b445b8ce50
Sajjad Alizadeh <sajjadalizadeh275800@gmail.com>
2021-10-05 18:26:12 +0330

    join_operator script added

--- scripts/join_operator.py
+++ scripts/join_operator.py
@@ -1,18 +1,21 @@
 from nltk import stem
 from nltk.tokenize import word_tokenize
-from scripts import list_files, check_path, folder_creator, slicer,save_json
+from scripts import list_files, check_path, folder_creator, slicer,save_json,list_files_and_sizes
 from scripts import base_script
 from pathlib import Path, PurePath
 import os
-import shutil
+import shutil,time
 
 
 def apply(from_path1,from_path2,name1,name2, to_path):
 
-    folder_name = str(name1+'_'+name2).replace('.zip','').replace('.rar','').replace('.7z','');
-    to_path = f'media/result/join/{folder_name}/'  # media/data/computer.zip/
+    folder_name = str(name1+'_'+name2).replace('.zip','').replace('.rar','').replace('.7z','');        
+    rand_int = round(time.time() * 1000)
+
+    to_path = f'media/result/join/{rand_int}/'  # media/data/computer.zip/
     folder_creator.apply(to_path)
 
+    result_list = []
 
     # file_list1 = list_files.apply(from_path1)
     # file_list2 = list_files.apply(from_path2)
@@ -54,7 +57,8 @@ def apply(from_path1,from_path2,name1,name2, to_path):
             print('copied', file_name)
 
  
-    result_list = []
     output_path = {'output_path':to_path }
     result_list.append(output_path)
+    result_list = result_list+list_files_and_sizes.apply(to_path)
+    print(result_list)
     return result_list
